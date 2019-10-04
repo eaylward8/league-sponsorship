b72e7958c33374be34d0dff013abc830c7f42149
Erik Aylward <eaylward@mail.med.upenn.edu>
2019-10-04 14:33:36 -0400

    Work on league model, controller

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,3 +1,20 @@
 class LeaguesController < ApplicationController
   def index; end
+
+  def new
+    @league = League.new
+  end
+
+  def create
+    @league = League.new(league_params)
+    return redirect_to action: :index if @league.save
+
+    render :new
+  end
+
+  private
+
+  def league_params
+    params.require(:league).permit(:name, :lat, :lng, :price)
+  end
 end
--- app/models/league.rb
+++ app/models/league.rb
@@ -1,5 +1,10 @@
 class League < ApplicationRecord
-  validates :name, :lat_long, :price, presence: true
+  validates :name, :lat, :lng, :price, presence: true
   validates :name, uniqueness: true
   validates :price, numericality: { greater_than: 0 }
+  # TODO: validation for lat/lng values
+
+  def location
+    [lat, lng]
+  end
 end
